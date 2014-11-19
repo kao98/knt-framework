79e8bb166bc440f015e45846b2339496e8a85e08
Kao ..98 <kao98@kaonet-fr.net>
2014-11-19 22:07:34 +0100

    Fix run-unit-tests batch shell scripts

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-php ./tests/units/mageekguy.atoum.phar -bf ./tests/units/atoum-bootstrap.php -d ./tests/units/Knt
+php ./tests/units/mageekguy.atoum.phar -bf ./tests/units/atoum-bootstrap.php -d ./tests/units
